9068d045f38a7de181680b20ecff22465e6e7b96
themez <xian.zeng@ruguoapp.com>
2016-09-17 23:02:58 +0800

    set raw message delivery after subscribing queue to topic

--- tests/cases/topic.js
+++ tests/cases/topic.js
@@ -35,18 +35,27 @@ test.serial('should bind queue', t => {
   t.context.sandbox.stub(sns, 'createTopic').callsArgWithAsync(1, null, { TopicArn: 'arn:aws-cn:sns:cn-north-1:abc:test_t1' })
   t.context.sandbox.stub(sqs, 'createQueue').callsArgWithAsync(1, null, { QueueUrl: 'http://test/tq1' })
 
-  const mock = t.context.sandbox.mock(sns).expects('subscribe').once()
-      .callsArgWithAsync(1, null, {})
+  const subStub = t.context.sandbox.mock(sns).expects('subscribe').once()
+      .callsArgWithAsync(1, null, { SubscriptionArn: 'arn:subscription' })
+  const setAttrStub = t.context.sandbox.stub(sns, 'setSubscriptionAttributes').callsArgWithAsync(1, null, {})
+
   const tq = new Queue('tq')
   const t2 = new Topic('t2')
   t2.subscribe(tq)
+
   return Promise.delay(200)
       .then(() => {
-        mock.verify()
-        t.deepEqual(mock.firstCall.args[0], {
+        t.truthy(subStub.calledOnce)
+        t.truthy(setAttrStub.calledOnce)
+        t.deepEqual(subStub.firstCall.args[0], {
           Protocol: 'sqs',
           TopicArn: 'arn:aws-cn:sns:cn-north-1:abc:test_t1',
           Endpoint: 'arn:sqs:test:test_tq',
+        })
+        t.deepEqual(setAttrStub.firstCall.args[0], {
+          SubscriptionArn: 'arn:subscription',
+          AttributeName: 'RawMessageDelivery',
+          AttributeValue: 'true',
         })
       })
 })
