80d07caf5408286dc30ec5e69b0b917cf0c3ce6a
Mingxin <shuataren@gmail.com>
2018-03-20 10:42:40 +0800

    Add meta data when send msg (#16)
    * add meta data about queue/topic at producer
    
    * bump version
    
    * fix test
    
    * fix version
    
    * remove redundant meta queue name
    
    * fix test

--- lib/producer.ts
+++ lib/producer.ts
@@ -17,7 +17,10 @@ class Producer {
    * Send message to topic.
    */
   async sendTopic<T = any>(topic: Topic, message: T): Promise<SNS.Types.PublishResponse> {
-    const encodedMessage = JSON.stringify(message)
+    const metaAttachedMessage = Object.assign({
+      _meta: { topicName: topic.name },
+    }, message)
+    const encodedMessage = JSON.stringify(metaAttachedMessage)
     return new Bluebird((resolve) => {
       if (topic.isReady) {
         resolve()
@@ -40,7 +43,8 @@ class Producer {
    * Send message to queue
    */
   async sendQueue<T = any>(queue: Queue, message: T, opts?: { DelaySeconds: number }): Promise<SQS.Types.SendMessageResult> {
-    const encodedMessage = JSON.stringify(message)
+    const metaAttachedMessage = Object.assign({ _meta: {} }, message)
+    const encodedMessage = JSON.stringify(metaAttachedMessage)
     return new Bluebird((resolve) => {
       if (queue.isReady) {
         resolve()
